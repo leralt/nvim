27c313f7c26e938eaf7b16635fdc7d34cc42c8b2
leralt <htliucl@foxmail.com>
2023-03-28 23:58:29 +0800

    update plugins

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    --目录树
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      window = {
+        position = "left",
+        width = 30,
+      },
+      filesystem = {
+        follow_current_file = true,
+        -- hijack_netrw_behavior = "open_current",
+      },
+    },
+  },
+}
---
+++ lua/plugins/symbols-outline.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    -- outline
+    "simrat39/symbols-outline.nvim",
+    cmd = "SymbolsOutline",
+    keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
+    config = true,
+  },
+}
