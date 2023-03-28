7e50b65e0bf43a1c7695f93436cb8c525e7c6025
leralt <htliucl@foxmail.com>
2023-03-28 17:39:19 +0800

    push github

---
+++ lua/plugins/rusttools.lua
@@ -0,0 +1,61 @@
+-- return {}
+return {
+    "simrat39/rust-tools.nvim",
+    opts = function() 
+        return {
+            tools = {
+                inlay_hints = {
+                    -- automatically set inlay hints (type hints)
+                    -- default: true
+                    auto = true,
+              
+                    -- Only show inlay hints for the current line
+                    only_current_line = false,
+              
+                    -- whether to show parameter hints with the inlay hints or not
+                    -- default: true
+                    show_parameter_hints = true,
+              
+                    -- prefix for parameter hints
+                    -- default: "<-"
+                    parameter_hints_prefix = "<- ",
+              
+                    -- prefix for all the other hints (type, chaining)
+                    -- default: "=>"
+                    other_hints_prefix = "=> ",
+              
+                    -- whether to align to the length of the longest line in the file
+                    max_len_align = false,
+              
+                    -- padding from the left if max_len_align is true
+                    max_len_align_padding = 1,
+              
+                    -- whether to align to the extreme right or not
+                    right_align = false,
+              
+                    -- padding from the right if right_align is true
+                    right_align_padding = 7,
+              
+                    -- The color of the hints
+                    highlight = "Comment",
+                  },
+            }
+        }
+    end
+}
+
+-- return {
+--     {
+--       "simrat39/rust-tools.nvim",
+--       dependencies = { "neovim/nvim-lspconfig", "nvim-lua/plenary.nvim", "mfussenegger/nvim-dap" },
+--       opts = function()
+--         local extension_path = vim.env.HOME .. "/.local/share/nvim/mason/packages/codelldb/extension/"
+--         local codelldb_path = extension_path .. "adapter/codelldb"
+--         local liblldb_path = extension_path .. "lldb/lib/liblldb.dylib"
+--         return {
+--           dap = { adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path) },
+--         }
+--       end,
+--     },
+--   }
+  
