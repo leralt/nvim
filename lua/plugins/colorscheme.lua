e7280df14f233f7637e01bfb306055d2f560a87b
leralt <htliucl@foxmail.com>
2023-04-18 23:33:06 +0800

    change theme to monokai_soda

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,24 @@
 return {
-    -- add gruvbox
-    { "ellisonleao/gruvbox.nvim" },
-  
-    -- Configure LazyVim to load gruvbox
-    {
-      "LazyVim/LazyVim",
-      opts = {
-        colorscheme = "gruvbox",
-      },
-    }
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
+
+  { "tanvirtin/monokai.nvim" },
+  -- {
+  --   "projekt0n/github-nvim-theme",
+  --   tag = "v0.0.7",
+  --   config = function()
+  --     require("github-theme").setup({
+  --       theme_style = "light",
+  --       -- other config
+  --     })
+  --   end,
+  -- },
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      -- colorscheme = "gruvbox",
+      colorscheme = "monokai_soda",
+    },
+  },
 }
